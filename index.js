05a926d54c8f024cedfd8558f9fbb260279776c7
Tarequl Islam <tareque.dev@gmail.com>
2022-05-22 16:56:36 +0600

    Order route done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -19,11 +19,28 @@ async function run() {
   try {
     await client.connect();
     const productCollection = client.db("alpha-climb").collection("products");
+    const ordersCollection = client.db("alpha-climb").collection("orders");
 
+    // GET == products
     app.get("/products", async (req, res) => {
       const query = {};
       const products = await productCollection.find(query).toArray();
       res.send(products);
+    });
+
+    // GET == single products
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+
+    // POST for order
+    app.post("/orders", async (req, res) => {
+      const orders = req.body;
+      const result = await ordersCollection.insertOne(orders);
+      res.send(result);
     });
   } finally {
   }
